5d0b2e5db3736cab0c6a99710cdd7458c41b5d74
Kara Leary <learykara@gmail.com>
2018-05-01 18:23:22 -1000

    add vue app skeleton

--- src/index.js
+++ src/index.js
@@ -2,9 +2,20 @@ import Vue from 'vue';
 
 require('./styles.less');
 
+Vue.component('sub-heading', {
+  props: {
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  template: '<h2>{{ content }}</h2>',
+});
+
 const vm = new Vue({
   el: '#app',
   data: {
-    message: "Hello, world!",
+    message: 'Hello, world!',
+    sub: { content: 'Nice to meet ya.' },
   },
 });
